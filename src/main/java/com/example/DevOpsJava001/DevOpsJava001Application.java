7b854faa07718de010f7807942bf65adcc3d5a16
teminelkan <teminelkan@gmail.com>
2019-08-14 10:22:50 -0500

    commit java codes

---
+++ src/main/java/com/example/DevOpsJava001/DevOpsJava001Application.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsJava001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsJava001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsJava001Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DevOpsJava001/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsJava001;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DevOpsJava001Application.class);
+	}
+
+}
